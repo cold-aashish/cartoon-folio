b05c09a7cd8e1f7bbcf9d932949d2e8b7f37832f
Aashish Risal <aashish@Aashishs-MacBook-Pro.local>
2024-03-18 14:08:17 +0545

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='text-blue-600 relative justify-center'>
+      
+    </section>
+  )
+}
+
+export default About
